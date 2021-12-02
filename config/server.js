f2fa71d2cac527707881a766529f0247a8d151e5
Aprisal Syam <samgokongdev@gmail.com>
2021-12-02 19:53:05 +0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '97cc773a04bd497deed45c8f0cc1ef63'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '24ff6210-3148-41bc-8ead-07edee0e84f8'
+};
